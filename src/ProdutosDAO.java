66546ffe0d42768675dc4c01b91935d8c8af0ddd
Breno Oliveira <brenomanu26@gmail.com>
2024-08-19 19:21:53 -0300

    listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -46,7 +46,39 @@ public class ProdutosDAO {
     }
     }
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+ ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+       conectaDAO conexao = new conectaDAO();
+         Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            // Usando o método connectDB para obter a conexão
+            conn = conexao.connectDB();
+            String sql = "SELECT * FROM produtos";
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+
+                listagem.add(produto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return listagem;
     }
     
